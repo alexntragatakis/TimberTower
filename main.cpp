47d57e8436fabe225a5d52efa73ca2310799b9ea
jalee <jalee4630@gmail.com>
2025-12-01 22:59:46 -0500

    Completed (?) SDP4

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main()
     FEHImage menuBg;
     float x_pos, y_pos; //coords clicked
     float x_trash, y_trash; //coords released at
+    int total_games = 0, total_blocks = 0, tallest_tower = 0;
 
     /* Background image for menu screen */
     menuBg.Open("MenuBG.png");
@@ -26,30 +27,85 @@ int main()
     LCD.SetFontScale(0.5);
     LCD.WriteAt("Play", 78, 128);
     LCD.WriteAt("How to Play", 192, 128);
-    LCD.WriteAt("Leaderboard", 55, 188);
+    LCD.WriteAt("Statistics", 55, 188);
     LCD.WriteAt("Credits", 203, 188);
 
-    /* Display next screen from menu */
-    while(!LCD.Touch(&x_pos, &y_pos)) {} // Wait for touch
-    while(LCD.Touch(&x_trash, &y_trash)); // Wait for release
-
-    if(x_pos > 40 && x_pos < 140 && y_pos > 115 && y_pos < 155) { // Play
-        LCD.Write("Play clicked");
-    }
-    else if(x_pos > 175 && x_pos < 275 && y_pos > 115 && y_pos < 155) { // How to Play
-        LCD.Write("How to Play clicked");
-    }
-    else if(x_pos > 40 && x_pos < 140 && y_pos > 175 && y_pos < 215) { // Leaderboard
-        LCD.Write("Leaderboard clicked");
-    }
-    else if(x_pos > 175 && x_pos < 275 && y_pos > 175 && y_pos < 215 ) { // Credits
-        LCD.Write("Credits clicked");
-    }
-
     while (1) {
-        
+        /* Display next screen from menu */
+        while(!LCD.Touch(&x_pos, &y_pos)) {} // Wait for touch
+        while(LCD.Touch(&x_trash, &y_trash)); // Wait for release
+
+        if(x_pos > 40 && x_pos < 140 && y_pos > 115 && y_pos < 155) { // Play        
+            LCD.SetFontColor(BLACK);
+            LCD.FillRectangle(0, 0, 500, 500);
+            LCD.SetFontColor(WHITE);
+            LCD.Write("Play game here");
+            LCD.WriteAt("< Quit >", 280, 200);
+        }
+        else if(x_pos > 175 && x_pos < 275 && y_pos > 115 && y_pos < 155) { // How to Play
+            LCD.SetFontColor(BLACK);
+            LCD.FillRectangle(0, 0, 500, 500);
+            LCD.SetFontColor(WHITE);
+            LCD.WriteAt("How to Play:", 120, 20);
+            LCD.WriteAt("You will be given a random block and its mass", 20, 70);
+            LCD.WriteAt("Stack the block on the platform or other blocks", 15, 90);
+            LCD.WriteAt("Keep the center of gravity of the tower centered", 15, 110);
+            LCD.WriteAt("If the tower becomes too unstable, it will fall", 20, 130);
+            LCD.WriteAt("< Quit >", 280, 200);
+        }
+        else if(x_pos > 40 && x_pos < 140 && y_pos > 175 && y_pos < 215) { // Stats/Leaderboard
+            LCD.SetFontColor(BLACK);
+            LCD.FillRectangle(0, 0, 500, 500);
+            LCD.SetFontColor(WHITE);
+            LCD.WriteAt("Statistics:", 120, 20);
+            LCD.WriteAt("Total Games Played:", 60, 70);
+            LCD.WriteAt("Tallest Tower:", 60, 100);
+            LCD.WriteAt("Total Blocks Stacked:", 60, 130);
+            /* Example stats */
+            LCD.WriteAt("4", 200, 70);
+            LCD.WriteAt("20 m", 200, 100);
+            LCD.WriteAt("52", 200, 130);
+            LCD.WriteAt("< Quit >", 280, 200);
+        }
+        else if(x_pos > 175 && x_pos < 275 && y_pos > 175 && y_pos < 215 ) { // Credits
+            LCD.SetFontColor(BLACK);
+            LCD.FillRectangle(0, 0, 500, 500);
+            LCD.SetFontColor(WHITE);
+            LCD.WriteAt("Credits:", 130, 20);
+            LCD.WriteAt("Developers:", 120, 70);
+            LCD.WriteAt("Alexander Ntragatakis and Jennifer Lee", 42, 90);
+            LCD.WriteAt("Menu Background Image:", 95, 120);
+            LCD.WriteAt("freepik.com", 120, 140);
+            LCD.WriteAt("< Quit >", 280, 200);
+        }
         LCD.Update();
-        // Never end
+        
+        while(!LCD.Touch(&x_pos, &y_pos)) {} // Wait for touch
+        while(LCD.Touch(&x_trash, &y_trash)); // Wait for release
+        if(x_pos > 280 && x_pos < 300 && y_pos > 200 && y_pos < 220) { // Quit
+            /* Background image for menu screen */
+            menuBg.Open("MenuBG.png");
+            menuBg.Draw(0, -10);
+            
+            /* Buttons on menu */
+            LCD.SetFontColor(LIGHTBLUE);
+            LCD.FillRectangle(40, 115, 100, 40);
+            LCD.FillRectangle(175, 115, 100, 40);
+            LCD.FillRectangle(40, 175, 100, 40);
+            LCD.FillRectangle(175, 175, 100, 40);
+            LCD.SetFontColor(WHITESMOKE);
+            LCD.DrawRectangle(40, 115, 100, 40);
+            LCD.DrawRectangle(175, 115, 100, 40);
+            LCD.DrawRectangle(40, 175, 100, 40);
+            LCD.DrawRectangle(175, 175, 100, 40);
+            LCD.SetFontColor(BLACK);
+            LCD.SetFontScale(0.5);
+            LCD.WriteAt("Play", 78, 128);
+            LCD.WriteAt("How to Play", 192, 128);
+            LCD.WriteAt("Statistics", 55, 188);
+            LCD.WriteAt("Credits", 203, 188);
+        }
+        LCD.Update();
     }
     return 0;
 }
