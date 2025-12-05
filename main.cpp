465819d0ef9c2d94ae001b86e374d93fb903e044
jalee <jalee4630@gmail.com>
2025-12-05 10:30:22 -0500

    Reverted changes

--- main.cpp
+++ main.cpp
@@ -40,34 +40,35 @@ int main()
     float x_trash, y_trash; //coords released at
     int total_games = 0, total_blocks = 0, tallest_tower = 0;
 
-    while (1) {
-        while(!LCD.Touch(&x_pos, &y_pos)) {} // Wait for touch
-        while(LCD.Touch(&x_trash, &y_trash)); // Wait for release
-        if(x_pos > 280 && x_pos < 300 && y_pos > 200 && y_pos < 220) { // Quit
-            /* Background image for menu screen */
-            menuBg.Open("MenuBG.png");
-            menuBg.Draw(0, -10);
-            
-            /* Buttons on menu */
-            LCD.SetFontColor(LIGHTBLUE);
-            LCD.FillRectangle(40, 115, 100, 40);
-            LCD.FillRectangle(175, 115, 100, 40);
-            LCD.FillRectangle(40, 175, 100, 40);
-            LCD.FillRectangle(175, 175, 100, 40);
-            LCD.SetFontColor(WHITESMOKE);
-            LCD.DrawRectangle(40, 115, 100, 40);
-            LCD.DrawRectangle(175, 115, 100, 40);
-            LCD.DrawRectangle(40, 175, 100, 40);
-            LCD.DrawRectangle(175, 175, 100, 40);
-            LCD.SetFontColor(BLACK);
-            LCD.SetFontScale(0.5);
-            LCD.WriteAt("Play", 78, 128);
-            LCD.WriteAt("How to Play", 192, 128);
-            LCD.WriteAt("Statistics", 55, 188);
-            LCD.WriteAt("Credits", 203, 188);
-        }
-        LCD.Update();
+    /* Background image for menu and game */
+    menuBg.Open("MenuBG.png");
+    menuBg.Draw(0, -10);
+    gameBg.Open("GameBG.png");
 
+    /* Images for building materials */
+    woodLong.Open("WoodPlankLong.png");
+    woodTall.Open("WoodPlankTall.png");
+    woodSquare.Open("WoodPlankSquare.png");
+    
+    /* Buttons on menu */ 
+    LCD.SetFontColor(LIGHTBLUE);
+    LCD.FillRectangle(40, 115, 100, 40);
+    LCD.FillRectangle(175, 115, 100, 40);
+    LCD.FillRectangle(40, 175, 100, 40);
+    LCD.FillRectangle(175, 175, 100, 40);
+    LCD.SetFontColor(WHITESMOKE);
+    LCD.DrawRectangle(40, 115, 100, 40);
+    LCD.DrawRectangle(175, 115, 100, 40);
+    LCD.DrawRectangle(40, 175, 100, 40);
+    LCD.DrawRectangle(175, 175, 100, 40);
+    LCD.SetFontColor(BLACK);
+    LCD.SetFontScale(0.5);
+    LCD.WriteAt("Play", 78, 128);
+    LCD.WriteAt("How to Play", 192, 128);
+    LCD.WriteAt("Statistics", 55, 188);
+    LCD.WriteAt("Credits", 203, 188);
+
+    while (1) {
         /* Display next screen from menu */
         while(!LCD.Touch(&x_pos, &y_pos)) {} // Wait for touch
         while(LCD.Touch(&x_trash, &y_trash)); // Wait for release
@@ -118,7 +119,32 @@ int main()
         }
         LCD.Update();
         
-        
+        while(!LCD.Touch(&x_pos, &y_pos)) {} // Wait for touch
+        while(LCD.Touch(&x_trash, &y_trash)); // Wait for release
+        if(x_pos > 280 && x_pos < 300 && y_pos > 200 && y_pos < 220) { // Quit
+            /* Background image for menu screen */
+            menuBg.Open("MenuBG.png");
+            menuBg.Draw(0, -10);
+            
+            /* Buttons on menu */
+            LCD.SetFontColor(LIGHTBLUE);
+            LCD.FillRectangle(40, 115, 100, 40);
+            LCD.FillRectangle(175, 115, 100, 40);
+            LCD.FillRectangle(40, 175, 100, 40);
+            LCD.FillRectangle(175, 175, 100, 40);
+            LCD.SetFontColor(WHITESMOKE);
+            LCD.DrawRectangle(40, 115, 100, 40);
+            LCD.DrawRectangle(175, 115, 100, 40);
+            LCD.DrawRectangle(40, 175, 100, 40);
+            LCD.DrawRectangle(175, 175, 100, 40);
+            LCD.SetFontColor(BLACK);
+            LCD.SetFontScale(0.5);
+            LCD.WriteAt("Play", 78, 128);
+            LCD.WriteAt("How to Play", 192, 128);
+            LCD.WriteAt("Statistics", 55, 188);
+            LCD.WriteAt("Credits", 203, 188);
+        }
+        LCD.Update();
     }
     return 0;
 }
